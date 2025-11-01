d3a4a20e3802c8d496dc6fc0fc4956223f884ed4
scarione <scarioneq@gmail.com>
2025-11-01 18:05:51 +0700

    Добавили модель автора

--- library_app/models.py
+++ library_app/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+#Модель автора
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    biography = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    died_date = models.DateField(null=True, blank=True)
+
+    #Представление в админке джанго
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+    #Метод, возвращающий строковое представление
+    def __str__(self):
+        return self.name
