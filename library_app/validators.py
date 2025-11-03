359610ad77f6dd12bccfdd437610babdd76b8027
scarione <scarioneq@gmail.com>
2025-11-03 19:32:04 +0700

    еле как переделали логику валидации

--- library_app/validators.py
+++ library_app/validators.py
@@ -1,52 +1,51 @@
 from rest_framework import serializers
 from .models import Book
 
-#ПЕРЕПИСАТЬ МБ НА СВИЧКЕЙС
-#придумать что делать с категорией худ.литра
 
-#Функция, валидирующая год публикации
-#!улучшить чтобы вместо 2025 был текущий год!
 def validate_publication_year(value):
-    if (value < 0) | value > 2025:
+    if (value < 0) | (value > 2025):
         raise serializers.ValidationError("Год публикации должен быть между 0 и 2025.")
     return value
 
-#валидация уникальности книги в зависимости от категории
+
+#Логика валидации:
+#Для худ литры и других категорий запрещаем только дубликаты по тайтл, автор, год, издательство (это сделано на уровне модели)
+#Для учебников запрещаем дубликаты по издательству, если уже есть учебник с названием и автором
+#Разрешаем создавать только учебники с разными годами публикации
+
 def validate_book_uniqueness(data, instance=None):
     title = data.get('title')
     author = data.get('author')
-    publication_year = data.get('publication_year')
     publisher = data.get('publisher')
     category = data.get('category')
 
-    #!после создания апи и тестирования проверю правильно ли это работает как я думаю, если че перепишу!
-    #!Мб надо будет сделать так, чтобы в список попадали данные из instance!
-    if not all([title, author, publication_year, publisher, category]):
-        return data
 
-    #Собираем все учебники из базы с одинаковыми полями названия, автора и издательства
+    #Логика для учебников
     if category == 'textbook':
-        existing_books = Book.objects.filter(
+        #Запрещаем создавать учебники с другим издательством, если уже есть учебник с таким тайлом и автором
+        existing_textbooks_same_title_author = Book.objects.filter(
             title=title,
             author=author,
-            publisher=publisher,
             category='textbook'
         )
 
-
-        #Исключаем из результата self книгу при обновлении. Если этой штуки не будет то мы всегда будем натыкаться на ошибку валидации
-        #при обновлении какой-то книги, ведь книга, которую мы обновляем уже есть в базе данных
+        #Если обновляем сущ. учебник, исключаем проверки для нее
         if instance:
-            existing_books = existing_books.exclude(pk=instance.pk)
+            existing_textbooks_same_title_author = existing_textbooks_same_title_author.exclude(pk=instance.pk)
 
-        #!это исключение оставляем!
-        #Если после всех ифок у нас остались книги, значит это дубликат и выбрасываем исключение
-        if existing_books.exists():
-            raise serializers.ValidationError({
-                'non_field_errors': [
-                    'Учебник с таким названием, автором и издательством уже существует. '
-                    'Можно добавлять только переиздания с разными годами публикации.'
-                ]
-            })
+        #Если нашли учебники с таким же названием и автором
+        if existing_textbooks_same_title_author.exists():
+            #Получаем все издательства этих учебников
+            existing_publishers = set(existing_textbooks_same_title_author.values_list('publisher', flat=True))
+
+            #Если пытаемся создать с издательством, которого нет в существующих выдаем ошибку
+            if publisher not in existing_publishers:
+                raise serializers.ValidationError({
+                    'publisher': [
+                        f'Нельзя создавать учебник с другим издательством. '
+                        f'Для учебника "{title}" автора {author.name} уже установлено издательство: {", ".join(existing_publishers)}. '
+                        f'Можно создавать только переиздания с разными годами публикации.'
+                    ]
+                })
 
     return data
